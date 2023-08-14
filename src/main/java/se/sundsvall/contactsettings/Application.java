b92bd7a4d11eae6b56b3d83415ec2042e18afe3b
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-08-14 10:16:09 +0200

    UF-5589: Lift to Dept44 2.1.2

--- src/main/java/se/sundsvall/contactsettings/Application.java
+++ src/main/java/se/sundsvall/contactsettings/Application.java
@@ -3,10 +3,10 @@ package se.sundsvall.contactsettings;
 import static org.springframework.boot.SpringApplication.run;
 
 import se.sundsvall.dept44.ServiceApplication;
-import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedReport;
+import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedCoverageReport;
 
 @ServiceApplication
-@ExcludeFromJacocoGeneratedReport
+@ExcludeFromJacocoGeneratedCoverageReport
 public class Application {
 	public static void main(final String... args) {
 		run(Application.class, args);
